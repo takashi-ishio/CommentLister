db22a22ed25f649d2fe6b98ce2b96eb2e3057ea1
Takashi Ishio <ishio@is.naist.jp>
2018-05-21 09:30:45 +0900

    Introduced ClassLoader

--- src/main/java/jp/naist/se/commentlister/ruby/CommentScanner.java
+++ src/main/java/jp/naist/se/commentlister/ruby/CommentScanner.java
@@ -1,20 +1,21 @@
 package jp.naist.se.commentlister.ruby;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.StringWriter;
 
 import org.jruby.embed.EmbedEvalUnit;
 import org.jruby.embed.ScriptingContainer;
 
 public class CommentScanner  {
+	
 
 	public CommentScanner(byte[] source) {
 		ScriptingContainer container = new ScriptingContainer();
 		StringWriter w = new StringWriter();
         //container.setOutput(w);
-    	try (FileReader f = new FileReader(new File("comment.rb"))) {
+		
+    	try (InputStreamReader f = new InputStreamReader(CommentScanner.class.getResourceAsStream("comment.rb"))) {
     		container.put("value", new String(source));
     		EmbedEvalUnit unit = container.parse(f, "comment.rb");
     		unit.run();
