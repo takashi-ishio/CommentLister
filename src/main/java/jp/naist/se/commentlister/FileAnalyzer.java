d464350b5ad1e07dbacd87f1933dae25aae1963b
Takashi Ishio <ishio@is.naist.jp>
2022-10-18 17:30:30 +0900

    Supported directories

--- src/main/java/jp/naist/se/commentlister/FileAnalyzer.java
+++ src/main/java/jp/naist/se/commentlister/FileAnalyzer.java
@@ -2,7 +2,11 @@ package jp.naist.se.commentlister;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
@@ -22,7 +26,8 @@ public class FileAnalyzer {
 			gen.writeStartObject();
 			gen.writeObjectFieldStart("Files");
 			for (String s: args) {
-				processFile(gen, s);
+				File f = new File(s);
+				processFile(gen, f.toPath());
 			}
 			gen.writeEndObject();
 		} catch (IOException e) {
@@ -30,17 +35,36 @@ public class FileAnalyzer {
 		}
 	}
 	
-	public static void processFile(JsonGenerator gen, String path) throws IOException {
-		FileType t = FileType.getFileType(path);
-		File f = new File(path);
-		if (!f.canRead()) {
-			System.err.println(path + " is not readable");
-			return;
-		}
-		byte[] content = Files.readAllBytes(f.toPath());
+	public static void processFile(JsonGenerator gen, Path path) throws IOException {
+		Files.walkFileTree(path, new FileVisitor<Path>() {
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				String filename = file.toString();
+				FileType t = FileType.getFileType(filename);
+				extractComments(gen, file, filename, t);
+				return FileVisitResult.CONTINUE;
+			}
+			@Override
+			public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+				System.err.println(file.toString() + " is not readable");
+				return FileVisitResult.CONTINUE;
+			}
+			@Override
+			public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+				return FileVisitResult.CONTINUE;
+			}
+			@Override
+			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+				return FileVisitResult.CONTINUE;
+			}
+		});
+	}
+	
+	public static void extractComments(JsonGenerator gen, Path path, String filename, FileType t) throws IOException {
+		byte[] content = Files.readAllBytes(path);
 		CommentReader comments = FileType.createCommentReader(t, content);
 		if (comments == null) return;
-		gen.writeObjectFieldStart(path);
+		gen.writeObjectFieldStart(filename);
 		gen.writeStringField("FileType", t.name());
 		int commentCount = 0;
 		while (comments.next()) {
@@ -52,6 +76,7 @@ public class FileAnalyzer {
 		}
 		gen.writeNumberField("CommentCount", commentCount);
 		gen.writeEndObject();
+		
 	}
 
 }
